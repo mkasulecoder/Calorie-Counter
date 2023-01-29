4eeece33fdf474b3b290f2462d9dc6c6ecb3c012
Mark Kasule <31680529+mkasulecoder@users.noreply.github.com>
2023-01-28 19:12:39 -0500

    Update script

--- CookieCalorieCounter.java
+++ CookieCalorieCounter.java
@@ -1,3 +1,9 @@
+/*
+A bag of cookies holds 40 cookies. 
+The calorie information on the bag claims that there are 10 servings
+ in the bag and that a serving equals 300 calories. Write a program that lets the user enter the number of cookies he or she actually ate and then reports the number of total calories consumed.
+*/
+
 /**
  * This program ask user the to enter how many cookies they ate,
  * then lets them know how many calories existed in those cookies
